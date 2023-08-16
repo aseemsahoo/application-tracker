d5f6b6db4fa5b686ccedb275de89e8103e23407b
aseemsahoo <aseemsahoo.v@gmail.com>
2023-08-17 00:12:49 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { SignupFormComponent } from './components/signup-form/signup-form.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    LoginFormComponent,
+    SignupFormComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
