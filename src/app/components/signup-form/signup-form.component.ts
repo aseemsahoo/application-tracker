6ab82d62f7c9e1a22292693c71d70c588d600a15
Aseem Sahoo <aseemsahoo.v@gmail.com>
2023-08-17 12:30:45 +0530

    Added ui services to notify the form to switch b/w windows

--- src/app/components/signup-form/signup-form.component.ts
+++ src/app/components/signup-form/signup-form.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signup-form',
   templateUrl: './signup-form.component.html',
   styleUrls: ['./signup-form.component.css']
 })
-export class SignupFormComponent {
-
+export class SignupFormComponent implements OnInit
+{
+  fullname: string;
+  email: string;
+  username: string;
+  password: string;
+  ngOnInit(): void {
+  }
+  onClick() : void
+  {
+    console.log(this.fullname + this.email + this.username + this.password);
+  }
 }
